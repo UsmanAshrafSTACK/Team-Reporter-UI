973893fdac712155cf2503d68d62ec777810a65e
Hamza-Mujahid <64688959+HassanNadeemWebD@users.noreply.github.com>
2021-11-26 22:32:20 +0500

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-light text-light bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand text-light"> <b> User Name : Hassan </b></a>
+          
+          <form className="d-flex">
+            
+           <h4> Logout</h4>
+          </form>
+        </div>
+      </nav>
+    </>
+  );
+}

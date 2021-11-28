d98c287de88edbdede7260a0ab74e823fcf837c7
Hassan079 <hn178139@gmail.com>
2021-11-29 00:40:49 +0500

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,19 @@
 import React from 'react'
-  
+
 
 function Home() {
   return (
-    <div className='container-fluid'>
-     <div className='container nav_container  '>
-         <h2 className='align-center'>Team You Own</h2>
-         </div>
-         <div className='container nav_container'>
-         <h2 className='align-center'>Team You Are Part Of</h2>
-         </div>
+    <>
+      <a href="#"><div className='container nav_container  '>
+        <h2 className='align-center'> Team You Own</h2>
+      </div></a>
+      <a href="#"> <div className='container nav_container'>
+        <h2 className='align-center'>Team You Are Part Of</h2>
+      </div></a>
 
 
-      
-    </div>
+    </>
+
   );
 }
 
